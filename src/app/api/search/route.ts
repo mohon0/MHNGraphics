513c689948f51346a7d5f0aa16b3a13d8f8a9ede
sejarparvez <sejarparvez@yahoo.com>
2024-10-20 15:03:13 +0600

    improve contact form

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -6,16 +6,10 @@ export async function GET(req: NextRequest) {
   try {
     const url = new URL(req.url);
     const queryParams = new URLSearchParams(url.search);
-
-    const page = queryParams.get("currentPage")
-      ? parseInt(queryParams.get("currentPage")!, 10)
-      : 1;
-
     const category = queryParams.get("category") || "all";
     const searchQuery = queryParams.get("searchQuery") || "";
 
-    const limit = 30;
-    const skip = (page - 1) * limit;
+    const limit = 5;
 
     let whereClause: {
       status?: DesignStatus;
@@ -38,8 +32,7 @@ export async function GET(req: NextRequest) {
 
     // Fetch designs with pagination and filtering
     const response = await Prisma.design.findMany({
-      where: whereClause, // Add the where clause here
-      skip,
+      where: whereClause,
       take: limit,
       orderBy: {
         createdAt: "desc",
@@ -54,22 +47,7 @@ export async function GET(req: NextRequest) {
       },
     });
 
-    // Get the total count of designs for pagination metadata with filtering
-    const totalCount = await Prisma.design.count({
-      where: whereClause, // Add the where clause here
-    });
-
-    const result = {
-      data: response,
-      meta: {
-        totalItems: totalCount,
-        totalPages: Math.ceil(totalCount / limit),
-        currentPage: page,
-        itemsPerPage: limit,
-      },
-    };
-
-    return NextResponse.json(result, { status: 200 });
+    return NextResponse.json(JSON.stringify(response), { status: 200 });
   } catch (error) {
     return new NextResponse("Internal Server Error", { status: 500 });
   }
--- src/components/pages/home/contact/Contact.tsx
+++ src/components/pages/home/contact/Contact.tsx
@@ -1,11 +1,49 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import img from "@/images/contact1.png";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." }),
+  phoneNumber: z
+    .string()
+    .min(10, { message: "Phone number must be at least 10 digits." }),
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  message: z
+    .string()
+    .min(10, { message: "Message must be at least 10 characters." }),
+});
 
 export default function Contact() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      phoneNumber: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
     <div className="mx-3 my-10 md:mx-10 lg:mx-32">
       <header className="text-center">
@@ -19,46 +57,95 @@ export default function Contact() {
             alt="Sejar Parvez"
             className="h-40 w-full rounded object-cover md:h-56"
           />
-
           <p className="mb-2 mt-4 text-3xl font-bold">Md. Mohon</p>
-
           <p className="mb-4 text-muted-foreground">
-            Feel free to get in touch with me for any inquiries, feedback or
-            assistance. I am dedicated to providing excellent service and are
+            Feel free to get in touch with me for any inquiries, feedback, or
+            assistance. I am dedicated to providing excellent service and am
             eager to hear from you.
           </p>
           <p>
-            Address: Rofi Tower 4th Floor Paira Chattra, Jhenaidah, Dhaka,
+            Address: Rofi Tower 4th Floor, Paira Chattra, Jhenaidah, Dhaka,
             Bangladesh
           </p>
           <p>Phone: +8801989-491248</p>
           <p>Email: contact@freelancermohon.com</p>
         </article>
 
-        <form className="dark:dark-bg col-span-1 flex flex-col rounded-lg bg-slate-50 p-4 md:col-span-7 md:p-6">
-          {[
-            { label: "Your Name", type: "text", id: "name" },
-            { label: "Phone Number", type: "text", id: "phone" },
-            { label: "Email", type: "email", id: "email" },
-          ].map(({ label, type, id }) => (
-            <div className="mb-4" key={id}>
-              <Label htmlFor={id}>{label}</Label>
-              <Input id={id} type={type} />
-            </div>
-          ))}
-          <div className="mb-4">
-            <Label
-              htmlFor="message"
-              className="text-sm font-semibold uppercase text-muted-foreground"
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="dark:dark-bg col-span-1 flex flex-col rounded-lg bg-slate-50 p-4 md:col-span-7 md:p-6"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Your Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Full Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone Number</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Phone Number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email Address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Your Message</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="min-h-40"
+                      placeholder="Your Message"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              aria-label="send message"
+              title="send message"
+              className="mt-10"
             >
-              Your Message
-            </Label>
-            <Textarea id="message" className="h-40" />
-          </div>
-          <Button aria-label="send message" title="send message">
-            SEND MESSAGE
-          </Button>
-        </form>
+              SEND MESSAGE
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
