fa76b0400d097d779a6366c836bc1302d3864cd5
sejarparvez <sejarparvez@yahoo.com>
2024-11-19 08:31:28 +0600

    made the new header responsive

--- src/components/layout/Header/SearchHeader.tsx
+++ src/components/layout/Header/SearchHeader.tsx
@@ -14,16 +14,19 @@ import {
 } from "@/components/ui/select";
 import { Skeleton } from "@/components/ui/skeleton";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { LucideSearch } from "lucide-react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense, useCallback, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import DesktopMenu from "./DesktopMenu";
+import User from "./User";
 
 function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const [disableAnimation, setDisableAnimation] = useState(false);
   const { scrollY } = useScroll();
   const [category, setCategory] = useState<string>(
     searchParams.get("category") || "all",
@@ -58,6 +61,26 @@ function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
     router.push(`/design?category=${category}&query=${searchQuery}&page=1`);
   }, [router, category, searchQuery]);
 
+  // Disable animation on small screens
+  useEffect(() => {
+    const handleResize = () => {
+      setDisableAnimation(window.innerWidth < 768); // Adjust breakpoint as needed
+    };
+
+    // Initial check
+    handleResize();
+    // Add resize listener
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setIsScrolled(latest > 50);
+  });
+
   return (
     <>
       <div>
@@ -69,34 +92,35 @@ function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
               </div>
             </div>
             <div className="flex items-center space-x-4">
-              <Link
-                href="#"
-                className="text-sm text-orange-500 hover:text-orange-600"
-              >
-                Pricing
+              <Link href="/company/pricing">
+                <Button
+                  className={`${fixed ? "border bg-transparent hover:bg-transparent" : "border border-primary bg-secondary text-primary hover:bg-secondary"}`}
+                >
+                  Pricing
+                </Button>
               </Link>
-              <Button variant="outline" size="sm">
-                Sign in
-              </Button>
+              <User />
             </div>
           </div>
         </div>
       </div>
 
       {/* Search Bar */}
-      <div className="sticky left-0 top-0 z-10 border-b bg-background">
+      <div className="left-0 top-0 z-10 border-b bg-background lg:sticky">
         <div className="container mx-auto px-4 py-2">
           <div className="flex items-center gap-2">
             <div className="flex items-center">
               <motion.div
                 className="mr-4"
                 initial={{ y: -56 }}
-                animate={{
-                  y: isScrolled ? 0 : -56,
-                  transition: {
-                    default: { type: "spring" },
-                  },
-                }}
+                animate={
+                  !disableAnimation
+                    ? {
+                        y: isScrolled ? 0 : -56,
+                        transition: { type: "spring" },
+                      }
+                    : {}
+                }
               >
                 <Link
                   href="/"
@@ -104,8 +128,10 @@ function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
                     fixed ? "text-white" : "text-black"
                   }`}
                 >
-                  <span className="text-2xl">MHN</span>
-                  <span className="text-xl">Graphics</span>
+                  <div className="flex items-end gap-2">
+                    <span className="text-2xl">MHN</span>
+                    <span className="hidden text-xl md:block">Graphics</span>
+                  </div>
                 </Link>
               </motion.div>
 
@@ -114,7 +140,7 @@ function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
                 onValueChange={handleFilterChange}
               >
                 <SelectTrigger
-                  className={`bg-background ${isScrolled ? "ml-10" : "-ml-44"} min-w-32 transition-all duration-200`}
+                  className={`bg-background ${isScrolled ? "md:ml-10" : "md:-ml-44"} -ml-20 min-w-32 transition-all duration-200`}
                 >
                   <SelectValue placeholder="Category" />
                 </SelectTrigger>
@@ -148,7 +174,10 @@ function SearchHeaderComponent({ fixed = false }: { fixed?: boolean }) {
                 }}
               />
               <Button onClick={handleSearch} type="submit">
-                Search
+                <span>
+                  <LucideSearch />
+                </span>
+                <span className="hidden md:block">Search</span>
               </Button>
             </div>
           </div>
